609597428fde91e83be84766142f99bb328f2f84
MilagrosMarcozzi <mmarcozzi1@mail.austral.edu.ar>
2025-04-11 12:31:46 -0300

    Update leap_year.py

--- leap_year.py
+++ leap_year.py
@@ -1,2 +1,11 @@
 def leap_year():
     print("TO DO")
+    año = int(input("Ingrese un año: "))
+    año_centenario = (año/100 == año//100) 
+    
+    if (año/4 == año//4) and (not año_centenario):
+        print(f"El año {año} es bisiesto")
+    elif año_centenario and (año/400 == año//400):
+        print(f"El año {año} es bisiesto")
+    else:
+        print(f"El año {año} no es bisiesto")
